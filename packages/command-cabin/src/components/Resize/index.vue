20f4cf3467b52c50bf35e39ea2facdec56299124
jinxiaochen <jinxiaochen@longfor.com>
2021-08-19 09:18:14 +0800

    feat: 添加resize

---
+++ packages/command-cabin/src/components/Resize/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Box extends Vue {
+  isDev = process.env.NODE_ENV === "development";
+
+  resizeHandle() {
+    const { clientWidth } = document.body;
+    document.body.style.transformOrigin = "left top";
+    document.body.style.transform = `scale(${clientWidth / 7680})`;
+  }
+
+  mounted() {
+    if (!this.isDev) return;
+    this.resizeHandle();
+    window.addEventListener("resize", this.resizeHandle);
+  }
+  unmounted() {
+    if (!this.isDev) return;
+    window.addEventListener("resize", this.resizeHandle);
+  }
+}
+</script>
