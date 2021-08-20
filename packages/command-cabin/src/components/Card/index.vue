1b2edf50a5f1ab8d8716054bd24a72e757fec6c2
jinxiaochen <jinxiaochen@longfor.com>
2021-08-20 09:39:01 +0800

    feat: 提取方块背景到CardDecorate

--- packages/command-cabin/src/components/Card/index.vue
+++ packages/command-cabin/src/components/Card/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="app-box">
-    <BoxBackground />
+    <!-- SVG 背景没有footer -->
+    <BoxBackground :showRectBackground="showRectBackground" />
     <div class="app-box__head">
       <slot name="title" v-bind="title">
         <h3 class="app-box__title">{{ title }}</h3>
       </slot>
     </div>
     <div class="app-box__body">
-      <div v-if="background" class="app-box__lattices"></div>
       <div class="app-box__content">
         <slot>
           <p>这里是body区域</p>
@@ -32,19 +32,18 @@ import BoxBackground from "@/components/BoxBackground/index.vue";
 })
 export default class Card extends Vue {
   /**
-   * 盒子标题
+   * 标题
    */
   @Prop({ default: "" }) title!: string;
 
   /**
    * 是否显示方格背景
    */
-  @Prop({ default: true }) background!: boolean;
+  @Prop({ default: true }) showRectBackground!: boolean;
 }
 </script>
 
 <style lang="scss" scoped>
-$box-opacity: 0.6;
 .app-box {
   position: relative;
   min-width: 580px;
@@ -62,40 +61,9 @@ $box-opacity: 0.6;
     color: #fff;
   }
 
-  &__background {
-    position: absolute;
-    overflow: hidden;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 100%;
-  }
-
-  &__lattices {
-    position: absolute;
-    top: 70px;
-    left: 20px;
-    right: 20px;
-    bottom: 0;
-    opacity: $box-opacity;
-    background-size: 100% 100%, 62px 62px;
-    background-repeat: no-repeat, repeat;
-    background-position: -2px -2px;
-    background-image: linear-gradient(
-        0,
-        rgba(5, 71, 143, 0.04) 0%,
-        rgba(1, 77, 159, 0.4) 50%,
-        rgba(5, 71, 143, 0.04) 80%
-      ),
-      url("~@/assets/img/box-lattices.png");
-  }
-
   &__body {
     position: relative;
-    margin-top: -48px;
-    padding-top: 85px;
-    padding-bottom: 30px;
+    padding: 30px 0;
   }
 
   &__content {
--- packages/command-cabin/src/components/CardDecorate/index.vue
+++ packages/command-cabin/src/components/CardDecorate/index.vue
@@ -29,6 +29,7 @@
         </defs>
       </svg>
     </div>
+    <div v-if="showRectBackground" class="app-box-background__lattices"></div>
     <div ref="wrapper" class="app-box-background__body">
       <svg
         :width="width"
@@ -41,11 +42,15 @@
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"
-            :d="bbbb"
+            :d="headBackgroundPath"
             fill="#113069"
             fill-opacity="0.6"
           />
-          <path :d="aaaa" stroke="url(#paint0_linear)" stroke-width="2" />
+          <path
+            :d="bodyBackgroundPath"
+            stroke="url(#paint0_linear)"
+            stroke-width="2"
+          />
         </g>
         <defs>
           <filter
@@ -89,31 +94,30 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Ref } from "vue-property-decorator";
+import { Component, Vue, Ref, Prop } from "vue-property-decorator";
 import { ResizeObserver } from "@juggle/resize-observer";
 
 @Component
-export default class Box extends Vue {
+export default class CardDecorate extends Vue {
   // @Prop({ default: 1000 }) width!: number;
-  // @Prop({ default: 1000 }) height!: number;
   @Ref() wrapper!: HTMLDivElement;
   /**
-   * 盒子最小宽度
+   * 是否显示方格背景
+   */
+  @Prop({ default: true }) showRectBackground!: boolean;
+
+  /**
+   * 背景最小宽度
    */
   MIN_WIDTH = 606;
 
   /**
-   * 背景最小高度
-   */
-  MIN_HEIGHT = 105;
-
-  /**
-   * 盒子默认宽度
+   * 背景默认宽度
    */
   width = 1000;
 
   /**
-   * 盒子默认高度
+   * 背景默认高度
    */
   height = 1000;
 
@@ -122,55 +126,27 @@ export default class Box extends Vue {
    */
   formatSvgPathD(d: string) {
     const w = (this.width - this.MIN_WIDTH) / 2 - 5;
-    const h = this.height - 505;
-
-    console.log(this.width, this.MIN_WIDTH, w);
+    const h = this.height;
 
     return d
       .replace(/{\d+(\.\d+)?}/g, (a: string) => {
+        // 匹配{100} 数字 * 宽度
         return `${w + parseFloat(`${a}`.slice(1, -1))}`;
       })
       .replace(/\[\d+(\.\d+)?\]/g, (a: string) => {
+        // 匹配[100] 数字 * 宽度
         return `${2 * w + parseFloat(`${a}`.slice(1, -1))}`;
       })
       .replace(/\(\d+(\.\d+)?\)/g, (a: string) => {
+        // 匹配(100) 数字 * 高度
         return `${h + parseFloat(`${a}`.slice(1, -1))}`;
       });
   }
 
-  get aaaa() {
-    return this.formatSvgPathD(`
-      M10 1.97314
-      H{46.1098}
-      L{90.5366} 56.1835
-      C{91.9353} 57.8903 {94.0272} 58.8797 {96.2352} 58.8797
-      H{129.433}
-      C{133.271} 58.8797 {136.863} 56.9967 {139.042} 53.842
-      C{140.848} 51.2286 {143.825} 49.6676 {147.006} 49.6676
-      H{468.873}
-      C{472.054} 49.6676 {475.031} 51.2286 {476.836} 53.842
-      C{479.015} 56.9967 {482.608} 58.8797 {486.445} 58.8797
-      H{521.25}
-      C{523.499} 58.8797 {525.275} 57.8468 {526.739} 56.45
-      C{528.167} 55.0882 {529.375} 53.3052 {530.494} 51.6548
-      L{530.527} 51.6054
-      C{531.236} 50.5604 {531.911} 49.565 {532.615} 48.6845
-      L{565.845} 7.10997
-      L{565.064} 6.48562
-      L{565.845} 7.10991
-      C{565.962} 6.96297 {566.079} 6.81343 {566.196} 6.66147
-      C{568.33} 3.88713 {571.367} 2 {574.679} 2
-      H[606]
-      C[610.972] 2 [615] 5.99679 [615] 10.9568
-      V484
-      C615 494.493 606.493 503 596 503
-      C5.02787 503 1 498.976 1 494.016
-      V10.9567
-      C1 5.99678 5.02789 2 10 2
-      Z`);
-  }
-
-  get bbbb() {
+  /**
+   * 头部标题svg path d
+   */
+  get headBackgroundPath() {
     return this.formatSvgPathD(`
       M{91.31} 55.5496
       L{46.5832} 0.973145
@@ -203,7 +179,42 @@ export default class Box extends Vue {
   }
 
   /**
-   * div大小改变更新svg宽度
+   * 内容区域svg path d
+   */
+  get bodyBackgroundPath() {
+    return this.formatSvgPathD(`
+      M10 1.97314
+      H{46.1098}
+      L{90.5366} 56.1835
+      C{91.9353} 57.8903 {94.0272} 58.8797 {96.2352} 58.8797
+      H{129.433}
+      C{133.271} 58.8797 {136.863} 56.9967 {139.042} 53.842
+      C{140.848} 51.2286 {143.825} 49.6676 {147.006} 49.6676
+      H{468.873}
+      C{472.054} 49.6676 {475.031} 51.2286 {476.836} 53.842
+      C{479.015} 56.9967 {482.608} 58.8797 {486.445} 58.8797
+      H{521.25}
+      C{523.499} 58.8797 {525.275} 57.8468 {526.739} 56.45
+      C{528.167} 55.0882 {529.375} 53.3052 {530.494} 51.6548
+      L{530.527} 51.6054
+      C{531.236} 50.5604 {531.911} 49.565 {532.615} 48.6845
+      L{565.845} 7.10997
+      L{565.064} 6.48562
+      L{565.845} 7.10991
+      C{565.962} 6.96297 {566.079} 6.81343 {566.196} 6.66147
+      C{568.33} 3.88713 {571.367} 2 {574.679} 2
+      H[606]
+      C[610.972] 2 [615] 5.99679 [615] 10.9568
+      V484
+      C615 494.493 606.493 503 596 503
+      C5.02787 503 1 498.976 1 494.016
+      V10.9567
+      C1 5.99678 5.02789 2 10 2
+      Z`);
+  }
+
+  /**
+   * 更新背景区域宽、高度
    */
   divSizeChangeHandle() {
     const { width, height } = this.wrapper.getBoundingClientRect();
@@ -216,6 +227,9 @@ export default class Box extends Vue {
    */
   mounted() {
     this.divSizeChangeHandle();
+    /**
+     * 监听容器尺寸变化
+     */
     const resizeObserver = new ResizeObserver(this.divSizeChangeHandle);
     resizeObserver.observe(this.wrapper);
   }
@@ -245,6 +259,24 @@ export default class Box extends Vue {
     left: 0;
     right: 0;
     bottom: 0;
+  }
+  &__lattices {
+    position: absolute;
+    top: 120px;
+    left: 20px;
+    right: 20px;
+    bottom: 50px;
+    opacity: 0.8;
+    background-size: 100% 100%, 62px 62px;
+    background-repeat: no-repeat, repeat;
+    background-position: -2px -2px;
+    background-image: linear-gradient(
+        0,
+        rgba(5, 71, 143, 0.04) 0%,
+        rgba(1, 77, 159, 0.4) 50%,
+        rgba(5, 71, 143, 0.04) 80%
+      ),
+      url("~@/assets/img/box-lattices.png");
   }
   svg {
     vertical-align: middle;
