ef2589b31d4207a28292ffa1b66bb3c4ba04c6b1
xiejiabin <xiejiabin@longfor.com>
2021-08-23 16:28:17 +0800

    feat: resize

--- packages/command-cabin/src/App.vue
+++ packages/command-cabin/src/App.vue
@@ -3,16 +3,13 @@
     <router-view />
     <!-- 若有初始化的请求 可以设置在未完成时页面转圈 -->
     <AppLoading v-if="false" />
-    <Resize />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Mixins, Provide } from "vue-property-decorator";
+import { Component, Mixins } from "vue-property-decorator";
 import AppLoading from "@/components/AppLoading.vue";
 import MixStore from "@/store/MixStore";
-import { setMiniProgramShare, showAppShare } from "./utils/guanyu";
-import { judgeDevice } from "@guanyu/h5-tools";
 import Resize from "@/components/Resize/index.vue";
 
 @Component({
@@ -23,48 +20,43 @@ import Resize from "@/components/Resize/index.vue";
   },
 })
 export default class App extends Mixins(MixStore) {
-  @Provide()
-  /**
-   * Provide会透传给所有自级组件
-   * 环境: APP 小程序 browser
-   */
-  visitSource = judgeDevice();
-
-  /**
-   * Provide会透传给所有自级组件
-   * 用Inject接收
-   */
-  @Provide()
-  share() {
-    const title = "冠寓";
-    const img =
-      "https://guanyuoss.oss-cn-qingdao.aliyuncs.com/prod/app/oNuSnxNLKKU.png";
-    if (this.visitSource === "小程序") {
-      return setMiniProgramShare({
-        title,
-        imageUrl: img,
-        path: location.href,
-      });
-    }
-    if (this.visitSource === "APP") {
-      /**
-       * 调用app的分享 分享目标到小程序 miniProgramUrl
-       */
-      return showAppShare(
-        {
-          title,
-          image: img,
-          url: location.href,
-        },
-        location.href,
-        2
+  resizeHandle() {
+    const app = this.$root.$el as HTMLDivElement;
+    const fixWidth = 7680;
+    const fixHeight = 3240;
+    const winWidth = window.innerWidth;
+    const winHeight = window.innerHeight;
+    const sx = winWidth / fixWidth;
+    const sy = winHeight / fixHeight;
+    console.log(sx, sy);
+    if (sx <= sy) {
+      app.style.setProperty(
+        "transform",
+        `translateY(${(winHeight - fixHeight * sx) / 2}px) scale(${sx})`
+      );
+    } else {
+      app.style.setProperty(
+        "transform",
+        `translateX(${(winWidth - fixWidth * sy) / 2}px) scale(${sy})`
       );
     }
+  }
+  created() {
+    // 7680 x 3240
+    window.removeEventListener("resize", this.resizeHandle);
+    window.addEventListener("resize", this.resizeHandle);
+  }
+  mounted() {
+    this.resizeHandle();
   }
 }
 </script>
 <style lang="scss">
 #app {
-  min-height: 100vh;
+  width: 7680px;
+  height: 3240px;
+  background: red;
+  transform-origin: 0 0;
+  overflow: hidden;
 }
 </style>
