79ca1a01ed54317e5fff9d02d33a0229e3c7d663
jinxiaochen <jinxiaochen@longfor.com>
2021-08-24 18:25:04 +0800

    feat: 示例

--- packages/command-cabin/src/views/Index.vue
+++ packages/command-cabin/src/views/Index.vue
@@ -3,26 +3,46 @@
     <div class="header"></div>
     <div class="main">
       <div class="main-left">
-        <Subtitle title="拓展盘面">
+        <SubWrapper title="拓展盘面">
           <template #extra>
             <a href="javascript:;">详情 →</a>
           </template>
-        </Subtitle>
-        <van-row gutter="24" style="margin: 0 48px">
-          <van-col style="width: 738px; height: 814px"><Card></Card></van-col>
-          <van-col style="width: 652px; height: 814px"><Card></Card></van-col>
-          <van-col style="width: 810px; height: 814px"><Card></Card></van-col>
-        </van-row>
-        <Subtitle title="营造盘面">
+          <van-row gutter="24">
+            <van-col>
+              <Card title="签约达成" style="width: 727px; height: 814px"></Card>
+            </van-col>
+            <van-col>
+              <Card title="商机储备" style="width: 640px; height: 814px"></Card>
+            </van-col>
+            <van-col>
+              <Card title="中央奖补" style="width: 804px; height: 814px"></Card>
+            </van-col>
+          </van-row>
+        </SubWrapper>
+        <SubWrapper title="营造盘面">
           <template #extra>
             <a href="javascript:;">详情 →</a>
           </template>
-        </Subtitle>
-        <van-row gutter="24">
-          <van-col style="width: 727px; height: 814px"><Card :showFooter="true"></Card></van-col>
-          <van-col style="width: 640px; height: 814px"><Card></Card></van-col>
-          <van-col style="width: 804px; height: 814px"><Card></Card></van-col>
-        </van-row>
+          <van-row gutter="24">
+            <van-col>
+              <Card
+                title="项目开业"
+                style="width: 1580px; height: 775px"
+              ></Card>
+            </van-col>
+            <van-col>
+              <Card title="产品品质" style="width: 616px; height: 775px"></Card>
+            </van-col>
+          </van-row>
+          <van-row gutter="24">
+            <van-col>
+              <Card
+                title="开业延期"
+                style="width: 2220px; height: 710px"
+              ></Card>
+            </van-col>
+          </van-row>
+        </SubWrapper>
       </div>
       <div class="main-center">main-center</div>
       <div class="main-right">main-right</div>
@@ -34,12 +54,12 @@
 <script lang="ts">
 import { Component } from "vue-property-decorator";
 import Base from "./Base";
-import Subtitle from "@/components/Subtitle/index.vue";
+import SubWrapper from "@/components/SubWrapper/index.vue";
 import Card from "@/components/Card/index.vue";
 
 @Component({
   components: {
-    Subtitle,
+    SubWrapper,
     Card,
   },
 })
