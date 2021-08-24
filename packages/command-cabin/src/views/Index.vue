a2c6347c039e09aee8dc6b36c38d6138a6f8293c
jinxiaochen <jinxiaochen@longfor.com>
2021-08-24 17:38:09 +0800

    feat: 调整最小高度

--- packages/command-cabin/src/views/Index.vue
+++ packages/command-cabin/src/views/Index.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="page__index">
-    <van-row>
-      <van-col span="8">
+    <div class="header"></div>
+    <div class="main">
+      <div class="main-left">
         <Subtitle title="拓展盘面">
           <template #extra>
-            <a href="#">详情→</a>
+            <a href="javascript:;">详情 →</a>
           </template>
         </Subtitle>
-      </van-col>
-      <van-col span="8">span: 8</van-col>
-      <van-col span="8">span: 8</van-col>
-    </van-row>
+        <van-row gutter="24" style="margin: 0 48px">
+          <van-col style="width: 738px; height: 814px"><Card></Card></van-col>
+          <van-col style="width: 652px; height: 814px"><Card></Card></van-col>
+          <van-col style="width: 810px; height: 814px"><Card></Card></van-col>
+        </van-row>
+        <Subtitle title="营造盘面">
+          <template #extra>
+            <a href="javascript:;">详情 →</a>
+          </template>
+        </Subtitle>
+        <van-row gutter="24">
+          <van-col style="width: 727px; height: 814px"><Card :showFooter="true"></Card></van-col>
+          <van-col style="width: 640px; height: 814px"><Card></Card></van-col>
+          <van-col style="width: 804px; height: 814px"><Card></Card></van-col>
+        </van-row>
+      </div>
+      <div class="main-center">main-center</div>
+      <div class="main-right">main-right</div>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
@@ -18,10 +35,12 @@
 import { Component } from "vue-property-decorator";
 import Base from "./Base";
 import Subtitle from "@/components/Subtitle/index.vue";
+import Card from "@/components/Card/index.vue";
 
 @Component({
   components: {
     Subtitle,
+    Card,
   },
 })
 export default class Index extends Base {}
@@ -30,5 +49,30 @@ export default class Index extends Base {}
 <style lang="scss" scoped>
 .page__index {
   width: 7680px;
+  color: #fff;
+  .header {
+    height: 100px;
+  }
+  .main {
+    display: flex;
+    margin: 0 60px;
+  }
+  .main-left {
+    max-width: 2300px;
+    min-width: 2300px;
+  }
+  .main-center {
+    flex: 1;
+  }
+  .main-right {
+    max-width: 2300px;
+    min-width: 2300px;
+  }
+
+  .main-left-row {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
