193402780d5edf1535709dfe4ea9686c00181f0d
jinxiaochen <jinxiaochen@longfor.com>
2021-08-24 18:34:49 +0800

    feat: 添加white-space组件

---
+++ packages/command-cabin/src/components/WhiteSpace/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-white-space"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class SubWrapper extends Vue {
+  /**
+   * 上下留白xs,sm,md,lg,xl
+   */
+  @Prop({ default: "lg" }) size!: "sm" | "md" | "lg" | "xs" | "xl";
+
+  get cls() {
+    return `app-white-space--${this.size}`;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-white-space {
+  clear: both;
+  &--sm {
+    height: 10px;
+  }
+  &--md {
+    height: 20px;
+  }
+  &--lg {
+    height: 30px;
+  }
+  &--xs {
+    height: 40px;
+  }
+  &--xl {
+    height: 50px;
+  }
+}
+</style>
