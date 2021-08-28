1554aae6cb16df5e7619f73ec45a42c0d6556824
jinxiaochen <jinxiaochen@longfor.com>
2021-08-28 15:40:38 +0800

    feat: 添加echarts

--- packages/command-cabin/src/plugins/echarts/index.ts
+++ packages/command-cabin/src/plugins/echarts/index.ts
@@ -1,59 +1,36 @@
 import * as echarts from "echarts/core";
 
 import {
-  BarChart,
-  // 系列类型的定义后缀都为 SeriesOption
-  LineChart,
-  LineSeriesOption,
-  GaugeChart,
-  GaugeSeriesOption,
-  GraphSeriesOption,
-  BarSeriesOption,
-  CustomChart,
-  CustomSeriesOption,
-  PieChart,
   MapChart,
+  LineSeriesOption,
   MapSeriesOption,
+  CustomSeriesOption,
+  BarSeriesOption,
 } from "echarts/charts";
 
 import {
   TitleComponent,
-  // 组件类型的定义后缀都为 ComponentOption
-  TitleComponentOption,
-  GridComponent,
-  GridComponentOption,
+  ToolboxComponent,
+  VisualMapComponent,
   TooltipComponent,
-  LegendComponent,
-  MarkLineComponent,
+  GeoComponent,
 } from "echarts/components";
 import { CanvasRenderer } from "echarts/renderers";
 
 // 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
 export type ECOption = echarts.ComposeOption<
-  | LineSeriesOption
-  | TitleComponentOption
-  | GridComponentOption
-  | GaugeSeriesOption
-  | GraphSeriesOption
-  | BarSeriesOption
-  | CustomSeriesOption
-  | MapSeriesOption
+  LineSeriesOption | BarSeriesOption | CustomSeriesOption | MapSeriesOption
 >;
 
 // 注册必须的组件
 echarts.use([
-  LineChart,
   TitleComponent,
   TooltipComponent,
-  GridComponent,
-  LegendComponent,
   CanvasRenderer,
-  MarkLineComponent,
-  BarChart,
-  GaugeChart,
-  CustomChart,
-  PieChart,
   MapChart,
+  VisualMapComponent,
+  ToolboxComponent,
+  GeoComponent,
 ]);
 
 export default echarts;
