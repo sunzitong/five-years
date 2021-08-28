0b3233924f0be3ad7fe5c14f52afc206bd1fd8b6
jinxiaochen <jinxiaochen@longfor.com>
2021-08-28 20:07:58 +0800

    feat: 添加滚动组件

---
+++ packages/command-cabin/src/components/Animationend/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="app-animated">
+    <ul>
+      <li
+        v-for="(item, index) in list"
+        :class="{ aaa: index === 0 }"
+        :key="`${item.id}`"
+      >
+        {{ item.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import { gsap } from "gsap";
+
+@Component({
+  components: {},
+})
+export default class Animationed extends Vue {
+  @Prop({ default: [] }) dataSource!: any[];
+
+  list: any[] = [];
+
+  @Watch("dataSource", { deep: true })
+  change(val: any) {
+    this.list = val;
+  }
+
+  move() {
+    this.$nextTick(() => {
+      gsap
+        .to(this.$el.querySelector(".aaa"), {
+          overflow: "hidden",
+          duration: 1,
+          scaleY: 0,
+          opacity: 0,
+        })
+        .delay(1)
+        .then(() => {
+          this.list.push(this.list.shift());
+          this.$nextTick(() => {
+            gsap
+              .to(this.$el.querySelectorAll("li"), {
+                scaleY: 1,
+                opacity: 1,
+                duration: 0,
+              })
+              .then(() => {
+                this.move();
+              });
+          });
+        });
+    });
+  }
+
+  mounted() {
+    this.list = this.dataSource;
+    this.move();
+  }
+}
+</script>
+
+<style lang="scss">
+ul {
+  height: 200px;
+  overflow: hidden;
+}
+</style>
