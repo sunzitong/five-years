e78700ee7afbf49b95540b09db2cc0e9f0918242
xiejiabin <xiejiabin@longfor.com>
2021-09-01 17:21:49 +0800

    feat: 缩放开关

--- packages/command-cabin/src/plugins/vant/index.ts
+++ packages/command-cabin/src/plugins/vant/index.ts
@@ -1,34 +1,13 @@
-import {
-  Toast,
-  Picker,
-  Icon,
-  Popup,
-  Overlay,
-  Lazyload,
-  Image,
-  Loading,
-  Button,
-  List,
-  Cell,
-  Dialog,
-  Row,
-  Col,
-} from "vant";
+import { Toast, Icon, Overlay, Loading, Dialog, Row, Col, Switch } from "vant";
 import Vue from "vue";
 
 Toast.allowMultiple(true);
 
 Vue.use(Toast);
-Vue.use(Picker);
 Vue.use(Icon);
-Vue.use(Popup);
 Vue.use(Overlay);
-Vue.use(Lazyload);
-Vue.use(Image);
 Vue.use(Loading);
-Vue.use(Button);
-Vue.use(List);
-Vue.use(Cell);
 Vue.use(Dialog);
 Vue.use(Row);
 Vue.use(Col);
+Vue.use(Switch);
