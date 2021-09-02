4c28233247405648439e5eab688bdaab368ccba8
xiejiabin <xiejiabin@longfor.com>
2021-09-02 18:51:52 +0800

    feat: 门店财务指标 A1

--- packages/command-cabin/src/views/Project/Index.vue
+++ packages/command-cabin/src/views/Project/Index.vue
@@ -1,146 +1,6 @@
 <template>
   <div class="page__index">
-    <div class="header"></div>
-    <div class="main">
-      <div class="main-left">
-        <SubWrapper title="拓展盘面">
-          <template #extra>
-            <a href="javascript:;">详情 →</a>
-          </template>
-          <van-row gutter="24">
-            <van-col>
-              <Card title="商机转化" style="width: 727px; height: 814px"></Card>
-            </van-col>
-            <van-col>
-              <Card title="项目获取" style="width: 640px; height: 814px"></Card>
-            </van-col>
-            <van-col>
-              <Card title="战略合作" style="width: 804px; height: 382px"></Card>
-              <WhiteSpace />
-              <van-row gutter="24">
-                <van-col>
-                  <Card
-                    title="中央奖补"
-                    size="small"
-                    style="width: 390px; height: 400px"
-                  ></Card>
-                </van-col>
-                <van-col>
-                  <Card
-                    title="投后攻坚"
-                    size="small"
-                    style="width: 390px; height: 400px"
-                  ></Card>
-                </van-col>
-              </van-row>
-            </van-col>
-          </van-row>
-        </SubWrapper>
-        <WhiteSpace />
-        <SubWrapper title="营造盘面">
-          <template #extra>
-            <a href="javascript:;">详情 →</a>
-          </template>
-          <van-row gutter="24">
-            <van-col>
-              <Card
-                title="项目开业"
-                style="width: 1580px; height: 660px"
-              ></Card>
-              <WhiteSpace />
-              <Card
-                title="开业延期"
-                style="width: 1580px; height: 670px"
-              ></Card>
-            </van-col>
-            <van-col>
-              <Card title="成本分析" style="width: 616px; height: 540px"></Card>
-              <WhiteSpace />
-              <Card title="产品品质" style="width: 616px; height: 370px"></Card>
-              <WhiteSpace />
-              <Card title="维保报事" style="width: 616px; height: 390px"></Card>
-            </van-col>
-          </van-row>
-        </SubWrapper>
-      </div>
-      <div class="main-center">main-center</div>
-      <div class="main-right">
-        <SubWrapper title="经营现状">
-          <template #extra>
-            <a href="javascript:;">详情 →</a>
-          </template>
-          <van-row gutter="24">
-            <van-col>
-              <Card
-                title="全业态收入及完成度"
-                style="width: 1565px; height: 480px"
-              ></Card>
-            </van-col>
-            <van-col>
-              <Card
-                title="平均续租率"
-                style="width: 631px; height: 480px"
-              ></Card>
-            </van-col>
-          </van-row>
-          <WhiteSpace />
-          <van-row gutter="24">
-            <van-col>
-              <van-row gutter="24">
-                <van-col>
-                  <Card
-                    title="经营性支出"
-                    style="width: 876px; height: 587px"
-                  ></Card>
-                </van-col>
-                <van-col>
-                  <Card
-                    title="成本使用进度"
-                    style="width: 562px; height: 587px"
-                  ></Card>
-                </van-col>
-              </van-row>
-              <WhiteSpace />
-              <van-row gutter="24">
-                <van-col>
-                  <Card
-                    title="客户洞察"
-                    style="width: 876px; height: 363px"
-                  ></Card>
-                </van-col>
-                <van-col>
-                  <Card
-                    title="设备离线率"
-                    style="width: 562px; height: 363px"
-                  ></Card>
-                </van-col>
-              </van-row>
-            </van-col>
-            <van-col>
-              <Card title="舆情风险" style="width: 734px; height: 978px"></Card>
-            </van-col>
-          </van-row>
-        </SubWrapper>
-        <WhiteSpace />
-        <SubWrapper title="舆情风险">
-          <template #extra>
-            <a href="javascript:;">详情 →</a>
-          </template>
-          <van-row gutter="24">
-            <van-col>
-              <Card title="收入预警" style="width: 706px; height: 688px"></Card>
-            </van-col>
-            <van-col>
-              <Card title="事件预警" style="width: 904px; height: 688px"></Card>
-            </van-col>
-            <van-col>
-              <Card title="监控预警" style="width: 561px; height: 688px"></Card>
-            </van-col>
-          </van-row>
-        </SubWrapper>
-      </div>
-    </div>
-    <div class="footer"></div>
+    <A1 />
   </div>
 </template>
 
@@ -150,12 +10,14 @@ import Base from "@/views/Base";
 import SubWrapper from "@/components/SubWrapper/index.vue";
 import Card from "@/components/Card/index.vue";
 import WhiteSpace from "@/components/WhiteSpace/index.vue";
+import A1 from "./components/A1.vue";
 
 @Component({
   components: {
     SubWrapper,
     Card,
     WhiteSpace,
+    A1,
   },
 })
 export default class Index extends Base {}
---
+++ packages/command-cabin/src/views/Project/components/A1.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="box">
+    <div class="title">
+      资产类型：
+      <span class="light">轻资产</span>
+    </div>
+    <ul class="list">
+      <li class="item">
+        <div class="label">开业间数</div>
+        <div class="value">
+          5343
+          <span class="unit">间</span>
+        </div>
+      </li>
+      <li class="item">
+        <div class="label">开业间数</div>
+        <div class="value">
+          5343
+          <span class="unit">间</span>
+        </div>
+      </li>
+      <li class="item">
+        <div class="label">开业间数</div>
+        <div class="value">
+          5343
+          <span class="unit">间</span>
+        </div>
+      </li>
+      <li class="item">
+        <div class="label">开业间数</div>
+        <div class="value">
+          5343
+          <span class="unit">间</span>
+        </div>
+      </li>
+      <li class="item">
+        <div class="label">开业间数</div>
+        <div class="value">
+          5343
+          <span class="unit">间</span>
+        </div>
+      </li>
+      <li class="item">
+        <div class="label">开业间数</div>
+        <div class="value">
+          5343
+          <span class="unit">间</span>
+        </div>
+        <div class="year">2021/01/01～2030/01/01</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class A1 extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+$light: #01f5f1;
+$gray: #b4b4b4;
+$padding-x: 82px;
+.box {
+  display: flex;
+  flex-flow: column nowrap;
+  color: #fff;
+  font-family: PingFang SC;
+  line-height: 1;
+  width: 1098px;
+  height: 560px;
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .item {
+    width: 50%;
+    height: 96px;
+    padding-left: $padding-x;
+    box-sizing: border-box;
+    margin-top: 50px;
+  }
+}
+.light {
+  color: $light;
+}
+.title {
+  font-weight: 500;
+  font-size: 36px;
+  padding-left: $padding-x;
+  margin-top: 30px;
+}
+.label {
+  font-size: 30px;
+}
+.value {
+  font-size: 60px;
+  color: $light;
+  margin-top: 6px;
+}
+.unit {
+  font-size: 24px;
+  color: $light;
+}
+.year {
+  font-size: 24px;
+  color: $gray;
+  margin-top: 6px;
+}
+</style>
