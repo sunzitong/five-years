128b9a97ebdb83af6087d743cad1d48c80c5ce07
xiejiabin <xiejiabin@longfor.com>
2021-09-02 16:27:12 +0800

    feat: 地图数据

--- packages/command-cabin/src/service/index.ts
+++ packages/command-cabin/src/service/index.ts
@@ -1,7 +1,8 @@
 import http from "./http";
-import { demoTest } from "./urls";
 import { env } from "@/store";
 import { IdField } from "@guanyu/shared";
+
+const BASE_URL = process.env.VUE_APP_BASE_API;
 
 /**
  * 测试数据
@@ -13,19 +14,65 @@ export type Demo = {
 };
 
 /**
- * 测试
+ * 测试 - 本地mock
  */
 export function getDemo() {
-  return http.get<Demo[]>(demoTest, {
+  return http.get<Demo[]>(`${BASE_URL}/demo/test`, {
     activityNumber: env.NODE_ENV,
   });
 }
 
+export interface MapJson {
+  type: string;
+  features: Feature[];
+}
+
+export interface Feature {
+  type: FeatureType;
+  properties: Properties;
+  geometry: Geometry;
+}
+
+export interface Geometry {
+  type: GeometryType;
+  coordinates: Array<Array<Array<number[] | number>>>;
+}
+
+export enum GeometryType {
+  MultiPolygon = "MultiPolygon",
+  Polygon = "Polygon",
+}
+
+export interface Properties {
+  adcode: number | string;
+  name: string;
+  center?: number[];
+  centroid?: number[];
+  childrenNum?: number;
+  level?: Level;
+  parent?: Parent;
+  subFeatureIndex?: number;
+  acroutes?: number[];
+  adchar?: string;
+}
+
+export enum Level {
+  Province = "province",
+}
+
+export interface Parent {
+  adcode: number;
+}
+
+export enum FeatureType {
+  Feature = "Feature",
+}
+
 /**
  * 获取地图数据
- * @param id
- * @returns
+ * @param id 行政编号
+ * @returns 地图geo数据
  */
 export function getMapJson(id: number) {
-  return http.get<any>(`/map-json/${id}.json`);
+  return (http.get(`/map-json/${id}.json`) as unknown) as Promise<MapJson>;
 }
