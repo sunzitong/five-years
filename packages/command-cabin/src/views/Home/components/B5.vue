35c4789d4674e12b598f32651bbabb7096f39ce3
jingxiaozhu <jingxiaozhu@longfor.com>
2021-09-04 18:36:12 +0800

    feat: 维修报事富文本

--- packages/command-cabin/src/views/Home/components/B5.vue
+++ packages/command-cabin/src/views/Home/components/B5.vue
@@ -4,9 +4,9 @@
       <div
         class="app-echarts"
         ref="wrapper"
-        style="width: 360px; height: 280px"
+        style="width: 690px; height: 280px"
       ></div>
-      <div class="pannel_values">
+      <!-- <div class="pannel_values">
         <div class="values_left">
           <div v-for="(value, index) in values" :key="index">
             {{ value }}
@@ -15,7 +15,7 @@
         <div class="values_right">
           <div v-for="(ratio, index) in ratios" :key="index">{{ ratio }}%</div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -23,6 +23,7 @@
 <script lang="ts">
 import { Component, Ref, Vue } from "vue-property-decorator";
 import echarts from "@/plugins/echarts";
+import { AnyObject, arrayToObject } from "@guanyu/shared";
 
 console.log(echarts);
 
@@ -42,28 +43,47 @@ export default class B5 extends Vue {
     {
       name: "电器类",
       value: 88,
+      ratio: 88,
     },
     {
       name: "硬装类",
       value: 88,
+      ratio: 12,
     },
     {
       name: "水暖类",
       value: 88,
+      ratio: 97,
     },
     {
       name: "渗漏类",
       value: 88,
+      ratio: 7,
     },
     {
       name: "设备类",
       value: 88,
+      ratio: 20,
     },
     {
       name: "其他",
       value: 88,
+      ratio: 20,
     },
   ];
+  objData1: AnyObject = {};
+  objData2: AnyObject = {};
+
+  created() {
+    this.objData1 = arrayToObject(this.data, {
+      key: "name",
+      value: "value",
+    });
+    this.objData2 = arrayToObject(this.data, {
+      key: "name",
+      value: "ratio",
+    });
+  }
 
   mounted() {
     const myChart = echarts.init(this.wrapper);
@@ -71,17 +91,18 @@ export default class B5 extends Vue {
     let option = {
       legend: {
         orient: "vertical",
-        left: "60%",
+        left: "45%",
         top: "0%",
         icon: "rec",
         itemWidth: 20,
         itemHeight: 20,
         itemGap: 18,
-        // data: this.data,
-        // formatter: function (value: any) {
-        //   const index = this.names.indexOf(value);
-        //   return `{a|${this.names[index]}}{b|${this.values[index]}%`;
-        // },
+        data: this.names,
+        formatter: (params: any) => {
+          console.log(1111111, this.objData1[params], params);
+
+          return `{a|${params}}{b|  ${this.objData1[params]}}{c| ${this.objData2[params]}%}`;
+        },
         textStyle: {
           color: "#FFFFFF",
           fontSize: 28,
@@ -89,12 +110,20 @@ export default class B5 extends Vue {
             a: {
               color: "#FFFFFF",
               fontSize: 28,
-              width: 84,
+              width: 90,
             },
             b: {
               color: "#01F5F1",
-              fontSize: 34,
-              width: 33,
+              fontSize: 30,
+              width: 100,
+              align: "right",
+              marginLeft: 10,
+            },
+            c: {
+              color: "#01F5F1",
+              fontSize: 30,
+              width: 100,
+              marginLeft: 39,
               align: "right",
             },
           },
