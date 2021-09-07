90937e3b190419ecbe3220b6378829e507c11bb3
xiejiabin <xiejiabin@longfor.com>
2021-09-07 16:51:20 +0800

    feat: 全局参数

--- packages/command-cabin/src/views/Base.ts
+++ packages/command-cabin/src/views/Base.ts
@@ -2,10 +2,4 @@ import { Component, Mixins } from "vue-property-decorator";
 import MixStore from "@/store/MixStore";
 
 @Component({})
-export default class Base extends Mixins(MixStore) {
-  created() {
-    if (process.env.NODE_ENV !== "production") {
-      window.PAGE = this;
-    }
-  }
-}
+export default class Base extends Mixins(MixStore) {}
