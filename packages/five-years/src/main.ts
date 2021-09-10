a502d02932ce70ec3856e031a3f496263fc92ebb
zhouwei <zhouwei201@longfor.com>
2021-09-10 18:47:38 +0800

    feat: add zhuge

--- packages/five-years/src/main.ts
+++ packages/five-years/src/main.ts
@@ -14,6 +14,28 @@ import "./directives";
 import "./filters";
 // 样式表
 import "./assets/css/style.scss";
+import { judgeDevice } from "@guanyu/h5-tools";
+import zhugeio from "@guanyu/zhugeio"; 
+const url = window.location.href;
+let appKey = "3ec10a66f82f4951ba811df964fcc199";
+let jsSrc = "https://dataintest.longfor.com/zhuge.js?v=";
+const visitSource = judgeDevice();
+if (url.includes("m.iguanyu.com")) {
+  appKey = "c6911bc459bb41a5acbb720e91057739"; // 正式
+  jsSrc = "https://datain.longfor.com/zhuge.js?v=";
+}
+zhugeio({
+  appKey,
+  jsSrc,
+  options: {
+    superProperty: {
+      访问来源: visitSource === "browser" ? "其他" : visitSource,
+    },
+  },
+});
+
+
+Vue.use(zhugeio);
 // console
 Vue.config.productionTip = false;
 Vue.config.ignoredElements = ["wx-open-launch-weapp"];
