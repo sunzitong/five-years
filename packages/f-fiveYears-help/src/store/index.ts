87782bc2196f777387584b7365aa4eaac7e4eaf5
sunzitong <642626517@qq.com>
2021-09-10 18:46:25 +0800

    feat: 初始化 助力仓库

---
+++ packages/f-fiveYears-help/src/store/index.ts
@@ -0,0 +1,5 @@
+import store from "./store";
+
+export const env = store.env;
+
+export default store;
---
+++ packages/f-fiveYears-help/src/utils/tools.ts
@@ -0,0 +1,31 @@
+/**
+ * 用 @guanyu/shared cloneJSON 代替
+ * @param obj 序列化JSON克隆对象
+ */
+
+/**
+ * 交换数组位置
+ * 用 @guanyu/shared swapArray 代替
+ * @param arr 源数组
+ * @param index1 索引1
+ * @param index2 索引2
+ */
+
+/**
+ * 判断字段是否存在
+ * 用 lodash/get 代替
+ * @param object 对象
+ * @param path 字符串表示的访问路径
+ */
+
+/**
+ * 获取线上环境
+ * 用 @guanyu/shared getEnvHost 代替
+ * @returns staging | test109 | development | production
+ */
+
+/**
+ * 获取url的query 包括hash模式的$route.query
+ * 用 @guanyu/shared getQueries 代替
+ * @param searchStr 网址的search部分 不传则当前网址
+ */
