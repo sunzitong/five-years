60abaa78291370c3492ac8731ba845cae43ad5a7
jinxiaochen <jinxiaochen@longfor.com>
2021-09-15 16:56:29 +0800

    feat: 更改SubWrapperA, CardA

--- packages/command-cabin/src/views/Project/Index.vue
+++ packages/command-cabin/src/views/Project/Index.vue
@@ -3,15 +3,15 @@
     <div class="header">龙湖冠寓重庆佳华北宸里店</div>
     <div class="main">
       <div class="main-left">
-        <SubWrapper title="财务相关指标及预估">
+        <SubWrapperA title="财务相关指标及预估">
           <van-row gutter="24">
             <van-col>
               <!-- 资产类型 -->
-              <Card style="width: 1098px; height: 560px">
+              <CardA style="width: 1098px; height: 560px">
                 <A1 />
-              </Card>
+              </CardA>
               <WhiteSpace />
-              <Card :showFooter="true" style="width: 1098px; height: 560px">
+              <CardA :showFooter="true" style="width: 1098px; height: 560px">
                 <template #footer>
                   <a>
                     投资任务书
@@ -19,145 +19,145 @@
                   </a>
                 </template>
                 <A2 />
-              </Card>
+              </CardA>
             </van-col>
             <van-col>
-              <Card style="width: 1218px; height: 1145px"><A6 /></Card>
+              <CardA style="width: 1218px; height: 1145px"><A6 /></CardA>
             </van-col>
           </van-row>
           <WhiteSpace />
           <van-row gutter="24">
             <van-col>
-              <Card
+              <CardA
                 title="经营团队"
                 size="small"
                 style="width: 764px; height: 408px"
               >
                 <A3 />
-              </Card>
+              </CardA>
             </van-col>
             <van-col>
-              <Card
+              <CardA
                 title="设计团队"
                 size="small"
                 style="width: 764px; height: 408px"
               >
                 <A4 />
-              </Card>
+              </CardA>
             </van-col>
             <van-col>
-              <Card
+              <CardA
                 title="施工团队"
                 size="small"
                 style="width: 764px; height: 408px"
               >
                 <A5 />
-              </Card>
+              </CardA>
             </van-col>
           </van-row>
           <WhiteSpace />
           <van-row>
             <van-col>
-              <Card
+              <CardA
                 title="项目区位"
                 style="width: 2340px; height: 1115px"
-              ></Card>
+              ></CardA>
             </van-col>
           </van-row>
-        </SubWrapper>
+        </SubWrapperA>
       </div>
       <div class="main-center">
-        <SubWrapper title="财务指标达成及预估">
+        <SubWrapperA title="财务指标达成及预估">
           <van-row gutter="24">
             <van-col>
               <WhiteSpace />
-              <Card style="width: 1506px; height: 2398px"></Card>
+              <CardA style="width: 1506px; height: 2398px"></CardA>
             </van-col>
             <van-col>
-              <Card title="综合经营指数" style="width: 1050px; height: 597px">
+              <CardA title="综合经营指数" style="width: 1050px; height: 597px">
                 <C5 />
-              </Card>
+              </CardA>
               <WhiteSpace />
-              <Card
+              <CardA
                 title="当前投资实现情况"
                 style="width: 1050px; height: 582px"
-              ></Card>
+              ></CardA>
               <WhiteSpace />
-              <Card
+              <CardA
                 title="奖补到账"
                 style="width: 1050px; height: 260px"
-              ></Card>
+              ></CardA>
             </van-col>
           </van-row>
-        </SubWrapper>
+        </SubWrapperA>
       </div>
       <div class="main-right">
-        <SubWrapper title="经营现状">
+        <SubWrapperA title="经营现状">
           <van-row gutter="24">
             <van-col>
-              <Card
+              <CardA
                 title="全业态收入及完成度"
                 style="width: 1593px; height: 578px"
               >
                 <template #data-cycle>{{ dataCycle }}</template>
-              </Card>
+              </CardA>
             </van-col>
             <van-col>
               <WhiteSpace />
-              <Card style="width: 723px; height: 555px">
+              <CardA style="width: 723px; height: 555px">
                 <D6 />
-              </Card>
+              </CardA>
             </van-col>
           </van-row>
           <WhiteSpace />
           <van-row gutter="24">
             <van-col>
-              <Card
+              <CardA
                 title="经营性支出"
                 style="width: 904px; height: 556px"
-              ></Card>
+              ></CardA>
             </van-col>
             <van-col>
-              <Card title="逾期金额" style="width: 665px; height: 556px"></Card>
+              <CardA title="逾期金额" style="width: 665px; height: 556px"></CardA>
             </van-col>
             <van-col>
-              <Card title="舆情风险" style="width: 723px; height: 556px">
+              <CardA title="舆情风险" style="width: 723px; height: 556px">
                 <D7 />
-              </Card>
+              </CardA>
             </van-col>
           </van-row>
           <WhiteSpace />
           <van-row gutter="24">
             <van-col>
-              <Card title="客户洞察" style="width: 904px; height: 392px"></Card>
+              <CardA title="客户洞察" style="width: 904px; height: 392px"></CardA>
             </van-col>
             <van-col>
-              <Card
+              <CardA
                 title="24小时设备离线率"
                 style="width: 665px; height: 392px"
               >
                 <D5 />
-              </Card>
+              </CardA>
             </van-col>
             <van-col>
-              <Card
+              <CardA
                 title="能源费用(收支差)"
                 style="width: 723px; height: 312px"
               >
                 <D8 />
-              </Card>
+              </CardA>
             </van-col>
           </van-row>
           <WhiteSpace />
           <van-row gutter="24">
             <van-col>
-              <Card
+              <CardA
                 title="风险预警"
                 style="width: 2340px; height: 1114px"
-              ></Card>
+              ></CardA>
             </van-col>
           </van-row>
-        </SubWrapper>
+        </SubWrapperA>
       </div>
     </div>
     <div class="footer">
@@ -189,8 +189,8 @@
 <script lang="ts">
 import { Component } from "vue-property-decorator";
 import Base from "@/views/Base";
-import SubWrapper from "@/components/SubWrapper/Index.vue";
-import Card from "@/components/Card/Index.vue";
+import SubWrapperA from "@/components/SubWrapperA/Index.vue";
+import CardA from "@/components/CardA/Index.vue";
 import WhiteSpace from "@/components/WhiteSpace/Index.vue";
 import A1 from "./components/A1.vue";
 import A2 from "./components/A2.vue";
@@ -210,8 +210,8 @@ import { DateScopes } from "@/service/analysis/commandCabin/publicEnum";
 
 @Component({
   components: {
-    SubWrapper,
-    Card,
+    SubWrapperA,
+    CardA,
     WhiteSpace,
     A1,
     A2,
