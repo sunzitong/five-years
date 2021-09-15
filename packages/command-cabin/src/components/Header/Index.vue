15f999e0f608c69e8b1444bca514adf0ac266f9d
jinxiaochen <jinxiaochen@longfor.com>
2021-09-15 15:33:02 +0800

    feat: 添加header

--- packages/command-cabin/src/components/Header/Index.vue
+++ packages/command-cabin/src/components/Header/Index.vue
@@ -2,221 +2,166 @@
   <div class="app-header">
     <div class="background">
       <svg
-        width="7680"
-        height="220"
-        viewBox="0 0 7680 220"
         fill="none"
+        viewBox="0 0 7692 226"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g opacity="0.8" :clip-path="`url(#${uuid}_clip0)`">
+        <mask
+          :id="`${uuid}_c`"
+          x="0"
+          y="0"
+          width="7692"
+          height="210"
+          fill="black"
+          maskUnits="userSpaceOnUse"
+        >
+          <rect width="7692" height="210" fill="#fff" />
           <path
-            fill-rule="evenodd"
+            d="m2842 6h-2836v196h2457.8c4.71 0 9.24-1.848 12.6-5.147l39.34-38.563c4.11-4.032 9.64-6.29 15.4-6.29h388.09l-92.73-113.34c-10.69-13.059-1.4-32.665 15.48-32.665zm1932 146 50.37-63.119v-0.0077l41.08-50.208c4.4-5.3794 5.42-11.87 3.9-17.643v-0.0147c-0.11-0.4911-0.25-0.9829-0.42-1.4753-0.78-2.259-1.96-4.3618-3.46-6.2044-3.35-4.1368-8.37-7.0009-14.43-7.3012-0.35-0.01734-0.7-0.02613-1.05-0.02613h2836v198h-2v-1h-2460.5c-4.37 0-8.6-1.593-11.88-4.483l-46.68-41.038c-4.01-3.531-9.17-5.479-14.52-5.479h-376.44z"
             clip-rule="evenodd"
-            d="M7680 0H0V198H2468L2515.72 148H5148.85L5212 198H7680V0Z"
-            :fill="`url(#${uuid}_paint0_linear)`"
+            fill-rule="evenodd"
           />
-          <rect
-            opacity="0.8"
-            width="7680"
-            height="87"
-            :fill="`url(#${uuid}_paint1_linear)`"
-          />
-          <g opacity="0.8" :filter="`url(#${uuid}_filter0_d)`">
-            <rect
-              y="87"
-              width="7680"
-              height="22"
-              :fill="`url(#${uuid}_paint2_linear)`"
-              shape-rendering="crispEdges"
-            />
-          </g>
-          <path
-            d="M7678 199H5210L5152 148H4766.5"
-            :stroke="`url(#${uuid}_paint3_linear)`"
-            stroke-width="4"
-            class="flex"
-          />
-          <path
-            d="M0 198H2466L2517 148H2915"
-            :stroke="`url(#${uuid}_paint4_linear)`"
-            stroke-width="4"
-            class="flex"
-          />
-          <path
-            d="M2822.69 30.7651C2813.6 19.6653 2821.5 3 2835.84 3H4843.85C4851.55 3 4857.69 7.96681 4859.95 14.5097L4862.78 13.5317L4859.95 14.5097C4861.16 18.0173 4860.56 21.6309 4858.77 25.478C4856.95 29.3749 4854.04 33.1863 4851.06 36.9234L4725.33 194.48C4723.76 196.451 4722.33 198.555 4720.98 200.597C4720.71 201.004 4720.44 201.407 4720.18 201.806C4719.1 203.446 4718.05 205.028 4716.95 206.554C4714.23 210.344 4711.43 213.391 4707.83 215.198C4705.51 216.36 4702.91 217 4700.21 217H4622.25C4612.21 217 4602.81 212.064 4597.11 203.797C4590.29 193.906 4579.04 188 4567.02 188H3111.25C3099.24 188 3087.99 193.906 3081.17 203.797C3075.47 212.064 3066.07 217 3056.02 217H2983.12C2978.02 217 2973.19 214.711 2969.96 210.765L2822.69 30.7651Z"
-            :fill="`url(#${uuid}_paint5_linear)`"
-            :stroke="`url(#${uuid}_paint6_linear)`"
-            stroke-width="6"
-          />
-          <path
-            d="M4843.77 0C4860.64 0 4869.93 19.6062 4859.25 32.6648L4711.97 212.665C4708.18 217.307 4702.49 220 4696.49 220H4623.59C4612.56 220 4602.23 214.579 4595.97 205.5C4595.79 205.234 4595.6 204.972 4595.41 204.713"
-            :stroke="`url(#${uuid}_paint7_linear)`"
-            stroke-width="6"
-          />
-          <path
-            d="M2835.84 0C2818.97 0 2809.68 19.6062 2820.36 32.6648L2967.64 212.665C2971.44 217.307 2977.12 220 2983.12 220H3056.02C3067.05 220 3077.38 214.579 3083.64 205.5C3083.82 205.234 3084.01 204.972 3084.2 204.713"
-            :stroke="`url(#${uuid}_paint8_linear)`"
-            stroke-width="6"
-          />
-        </g>
+        </mask>
+        <path
+          d="m2842 6h-2836v196h2457.8c4.71 0 9.24-1.848 12.6-5.147l39.34-38.563c4.11-4.032 9.64-6.29 15.4-6.29h388.09l-92.73-113.34c-10.69-13.059-1.4-32.665 15.48-32.665zm1932 146 50.37-63.119v-0.0077l41.08-50.208c4.4-5.3794 5.42-11.87 3.9-17.643v-0.0147c-0.11-0.4911-0.25-0.9829-0.42-1.4753-0.78-2.259-1.96-4.3618-3.46-6.2044-3.35-4.1368-8.37-7.0009-14.43-7.3012-0.35-0.01734-0.7-0.02613-1.05-0.02613h2836v198h-2v-1h-2460.5c-4.37 0-8.6-1.593-11.88-4.483l-46.68-41.038c-4.01-3.531-9.17-5.479-14.52-5.479h-376.44z"
+          clip-rule="evenodd"
+          fill="#000"
+          fill-opacity=".4"
+          fill-rule="evenodd"
+        />
+        <path
+          d="m6 6v-6h-6v6h6zm0 196h-6v6h6v-6zm2470.4-5.147 4.2 4.285-4.2-4.285zm39.34-38.563-4.2-4.284 4.2 4.284zm403.49-6.29v6h12.66l-8.02-9.799-4.64 3.799zm-92.73-113.34 4.64-3.7994-4.64 3.7994zm1997.9 50.216-4.62-3.8262-0.04 0.0416-0.03 0.0422 4.69 3.7424zm-50.37 63.119-4.69-3.742-7.77 9.742h12.46v-6zm50.37-63.127 4.62 3.8261 0.02-0.0133 0.01-0.0134-4.65-3.7994zm41.08-50.208-4.64-3.7994 4.64 3.7994zm3.9-17.643-5.84 1.3599 0.02 0.0802 0.02 0.0797 5.8-1.5198zm0-0.0147-5.85 1.3542 0.01 0.0057 5.84-1.3599zm-0.42-1.4753-5.68 1.9556v0.0018l5.68-1.9574zm-3.46-6.2044-4.67 3.7737 0.01 0.0106 0.01 0.0105 4.65-3.7948zm-14.43-7.3012-0.31 5.9925 0.01 2e-4 0.3-5.9927zm2834.9-0.02613h6v-6h-6v6zm0 198v6h6v-6h-6zm-2 0h-6v6h6v-6zm0-1h6v-6h-6v6zm-2472.3-4.483-3.97 4.506 3.97-4.506zm-46.68-41.038-3.96 4.505 3.96-4.505zm-5159-145.48h2836v-12h-2836v12zm6 190v-196h-12v196h12zm2451.8-6h-2457.8v12h2457.8v-12zm8.4-3.431c-2.24 2.199-5.26 3.431-8.4 3.431v12c6.28 0 12.32-2.464 16.8-6.862l-8.4-8.569zm39.34-38.563-39.34 38.563 8.4 8.569 39.34-38.563-8.4-8.569zm19.6-8.006c-7.33 0-14.37 2.874-19.6 8.006l8.4 8.569c2.99-2.933 7.01-4.575 11.2-4.575v-12zm388.09 0h-388.09v12h388.09v-12zm-97.38-103.54 92.73 113.33 9.29-7.598-92.73-113.34-9.29 7.5988zm20.13-42.464c-21.94 0-34.02 25.488-20.13 42.464l9.29-7.5988c-7.48-9.1411-0.97-22.865 10.84-22.865v-12zm1977.7 85.138-50.37 63.12 9.38 7.484 50.37-63.119-9.38-7.4847zm0.07-0.0915v0.0077l9.24 7.6523v-0.0077l-9.24-7.6523zm41.06-50.182-41.08 50.208 9.29 7.5988 41.08-50.208-9.29-7.5988zm2.74-12.324c1.06 4.0401 0.37 8.526-2.74 12.324l9.29 7.5989c5.69-6.961 7.02-15.456 5.06-22.962l-11.61 3.0397zm-0.04-0.1746v0.0147l11.69-2.7199-0.01-0.0147-11.68 2.7199zm-0.26-0.8778c0.11 0.2921 0.19 0.5825 0.25 0.872l11.7-2.7083c-0.17-0.6926-0.36-1.3858-0.6-2.0785l-11.35 3.9148zm-2.43-4.367c1.06 1.2998 1.89 2.7811 2.43 4.3652l11.35-3.9112c-1.01-2.9339-2.54-5.6583-4.49-8.0435l-9.29 7.5895zm-10.08-5.1033c4.26 0.2109 7.72 2.1922 10.06 5.0822l9.33-7.5474c-4.36-5.3836-10.92-9.1305-18.8-9.5202l-0.59 11.985zm-0.75-0.0188c0.25 0 0.5 0.0063 0.74 0.0186l0.61-11.985c-0.45-0.022384-0.9-0.033679-1.35-0.033679v12zm2836-12h-2836v12h2836v-12zm6 204v-198h-12v198h12zm-8 6h2v-12h-2v12zm-6-7v1h12v-1h-12zm-2454.5 6h2460.5v-12h-2460.5v12zm-15.85-5.977c4.39 3.852 10.02 5.977 15.85 5.977v-12c-2.91 0-5.73-1.062-7.92-2.988l-7.93 9.011zm-46.67-41.039 46.67 41.039 7.93-9.011-46.67-41.039-7.93 9.011zm-10.56-3.984c3.89 0 7.64 1.416 10.56 3.984l7.93-9.011c-5.11-4.494-11.69-6.973-18.49-6.973v12zm-376.44 0h376.44v-12h-376.44v12z"
+          :fill="`url(#${uuid}_h)`"
+          :mask="`url(#${uuid}_c)`"
+        />
+        <path
+          d="m2842 6c-16.88 0-26.17 19.606-15.48 32.665l147.27 180c3.8 4.642 9.48 7.335 15.48 7.335h72.91c11.03 0 21.35-5.421 27.61-14.5s16.59-14.5 27.62-14.5h1455.8c11.03 0 21.35 5.421 27.61 14.5s16.59 14.5 27.62 14.5h77.95c3.18 0 6.24-0.753 8.97-2.12 8.51-4.268 12.56-14.086 18.5-21.529l125.73-157.56c5.94-7.4431 12.48-16.26 9.38-25.263-2.65-7.6646-9.87-13.532-18.94-13.532h-2008z"
+          clip-rule="evenodd"
+          fill="#000"
+          fill-opacity=".4"
+          fill-rule="evenodd"
+        />
+        <path
+          d="m2842 6h18.15v6h-18.15c-11.81 0-18.32 13.724-10.84 22.865l147.27 180c2.66 3.25 6.64 5.135 10.84 5.135h72.91c9.05 0 17.53-4.451 22.67-11.906 7.38-10.703 19.55-17.094 32.56-17.094h1457.2c13.01 0 25.18 6.391 32.56 17.094 5.14 7.455 13.62 11.906 22.67 11.906h72.91c4.2 0 8.18-1.885 10.84-5.135l147.27-180c7.48-9.1411 0.97-22.865-10.84-22.865h-18.15v-6h18.15c16.88 0 26.17 19.606 15.48 32.665l-147.27 180c-3.8 4.642-9.48 7.335-15.48 7.335h-72.91c-11.03 0-21.35-5.421-27.61-14.5s-16.59-14.5-27.62-14.5h-1457.2c-11.03 0-21.36 5.421-27.62 14.5s-16.58 14.5-27.61 14.5h-72.91c-6 0-11.68-2.693-15.48-7.335l-147.27-180c-10.69-13.059-1.4-32.665 15.48-32.665z"
+          clip-rule="evenodd"
+          :fill="`url(#${uuid}_g)`"
+          fill-rule="evenodd"
+        />
+        <path
+          d="m3202.2 191v6h-84.74c-11.03 0-21.36 5.421-27.62 14.5s-16.58 14.5-27.61 14.5h-72.91c-6 0-11.68-2.693-15.48-7.335l-147.27-180c-10.69-13.059-1.4-32.665 15.48-32.665h18.15v6h-18.15c-11.81 0-18.32 13.724-10.84 22.865l147.27 180c2.66 3.25 6.64 5.135 10.84 5.135h72.91c9.05 0 17.53-4.451 22.67-11.906 7.38-10.703 19.55-17.094 32.56-17.094h84.74z"
+          clip-rule="evenodd"
+          :fill="`url(#${uuid}_f)`"
+          fill-rule="evenodd"
+        />
+        <path
+          d="m4489.8 191v6h84.74c11.03 0 21.36 5.421 27.62 14.5s16.58 14.5 27.61 14.5h72.91c6 0 11.68-2.693 15.48-7.335l147.27-180c10.69-13.059 1.4-32.665-15.48-32.665h-18.15v6h18.15c11.81 0 18.32 13.724 10.84 22.865l-147.27 180c-2.66 3.25-6.64 5.135-10.84 5.135h-72.91c-9.05 0-17.53-4.451-22.67-11.906-7.38-10.703-19.55-17.094-32.56-17.094h-84.74z"
+          clip-rule="evenodd"
+          :fill="`url(#${uuid}_e)`"
+          fill-rule="evenodd"
+        />
+        <path
+          d="m4375 198h-1058v-6h1058v6z"
+          clip-rule="evenodd"
+          :fill="`url(#${uuid}_d)`"
+          fill-rule="evenodd"
+        />
+        <path d="m2804 174h24l-32 36h-24l32-36z" :fill="`url(#${uuid}_a)`" />
+        <path d="m2736 210 32-36h-24l-32 36h24z" :fill="`url(#${uuid}_a)`" />
+        <path d="m2652 210 32-36h24l-32 36h-24z" :fill="`url(#${uuid}_a)`" />
+        <path d="m2648 174h-24l-32 36h24l32-36z" :fill="`url(#${uuid}_a)`" />
+        <path d="m2588 174h-24l-32 36h24l32-36z" :fill="`url(#${uuid}_a)`" />
+
+        <path d="m4868 174h-24l32 36h24l-32-36z" :fill="`url(#${uuid}_b)`" />
+        <path d="m4936 210-32-36h24l32 36h-24z" :fill="`url(#${uuid}_b)`" />
+        <path d="m5020 210-32-36h-24l32 36h24z" :fill="`url(#${uuid}_b)`" />
+        <path d="m5024 174h24l32 36h-24l-32-36z" :fill="`url(#${uuid}_b)`" />
+        <path d="m5084 174h24l32 36h-24l-32-36z" :fill="`url(#${uuid}_b)`" />
         <defs>
-          <filter
-            :id="`${uuid}_filter0_d`"
-            x="-2"
-            y="87"
-            width="7684"
-            height="28"
-            filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB"
-          >
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dy="4" />
-            <feGaussianBlur stdDeviation="1" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0.00392157 0 0 0 0 0.0745098 0 0 0 0 0.333333 0 0 0 1 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow"
-              result="shape"
-            />
-          </filter>
           <linearGradient
-            :id="`${uuid}_paint0_linear`"
-            x1="4187"
-            y1="99"
-            x2="4187"
-            y2="198"
+            :id="`${uuid}_h`"
+            x1="754"
+            x2="6842"
+            y1="204.01"
+            y2="204.01"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#01175B" />
-            <stop offset="0.968759" stop-color="#060745" />
+            <stop stop-color="#193497" stop-opacity="0" offset="0" />
+            <stop stop-color="#193497" offset=".27135" />
+            <stop stop-color="#5180E4" offset=".49219" />
+            <stop stop-color="#193497" offset=".6776" />
+            <stop stop-color="#193497" stop-opacity="0" offset="1" />
           </linearGradient>
           <linearGradient
-            :id="`${uuid}_paint1_linear`"
-            x1="3840"
-            y1="-15.5"
-            x2="3840"
-            y2="115.5"
+            :id="`${uuid}_g`"
+            x1="3846"
+            x2="3846"
+            y1="6"
+            y2="226"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#000C4B" />
-            <stop offset="0.489583" stop-color="#02256F" />
-            <stop offset="0.942708" stop-color="#000D4C" />
+            <stop stop-color="#4F72E4" stop-opacity="0" offset="0" />
+            <stop stop-color="#193497" offset="1" />
           </linearGradient>
           <linearGradient
-            :id="`${uuid}_paint2_linear`"
-            x1="3840"
-            y1="87"
-            x2="3840"
-            y2="109"
+            :id="`${uuid}_f`"
+            x1="2864.2"
+            x2="3012.1"
+            y1="34"
+            y2="226"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#02246D" />
-            <stop offset="1" stop-color="#01216A" stop-opacity="0" />
+            <stop stop-color="#193497" stop-opacity="0" offset="0" />
+            <stop stop-color="#5181E6" offset=".47082" />
+            <stop stop-color="#4069CE" offset=".6933" />
+            <stop stop-color="#193497" offset="1" />
           </linearGradient>
           <linearGradient
-            :id="`${uuid}_paint3_linear`"
-            x1="48.0003"
-            y1="198.974"
-            x2="7678"
-            y2="198.974"
+            :id="`${uuid}_e`"
+            x1="4827.8"
+            x2="4679.9"
+            y1="34"
+            y2="226"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset="0.133765" stop-color="#01C5D0" stop-opacity="0.2" />
-            <stop offset="0.543349" stop-color="#01F5F1" />
-            <stop offset="0.892314" stop-color="#01C5D0" stop-opacity="0.2" />
+            <stop stop-color="#193497" stop-opacity="0" offset="0" />
+            <stop stop-color="#5181E6" offset=".47082" />
+            <stop stop-color="#4069CE" offset=".6933" />
+            <stop stop-color="#193497" offset="1" />
           </linearGradient>
           <linearGradient
-            :id="`${uuid}_paint4_linear`"
-            x1="48"
-            y1="198.974"
-            x2="7678"
-            y2="198.974"
+            :id="`${uuid}_d`"
+            x1="3317"
+            x2="4367.2"
+            y1="192"
+            y2="192"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset="0.133765" stop-color="#01C5D0" stop-opacity="0.2" />
-            <stop offset="0.543349" stop-color="#01F5F1" />
-            <stop offset="0.892314" stop-color="#01C5D0" stop-opacity="0.2" />
+            <stop stop-color="#193498" stop-opacity="0" offset="0" />
+            <stop stop-color="#5485EC" offset=".49089" />
+            <stop stop-color="#1D358C" stop-opacity="0" offset="1" />
           </linearGradient>
           <linearGradient
-            :id="`${uuid}_paint5_linear`"
-            x1="3839.84"
-            y1="0"
-            x2="3839.84"
-            y2="220"
+            :id="`${uuid}_a`"
+            x1="2844"
+            x2="2532"
+            y1="174"
+            y2="174"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#02246C" />
-            <stop offset="1" stop-color="#07003C" />
+            <stop stop-color="#193497" offset="0" />
+            <stop stop-color="#5180E4" stop-opacity=".18" offset="1" />
           </linearGradient>
           <linearGradient
-            :id="`${uuid}_paint6_linear`"
-            x1="2838.81"
-            y1="87.5002"
-            x2="4827.31"
-            y2="91.5001"
+            :id="`${uuid}_b`"
+            x1="4828"
+            x2="5140"
+            y1="174"
+            y2="174"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset="0.0131516" stop-color="#01E7E7" stop-opacity="0" />
-            <stop
-              offset="0.499477"
-              stop-color="#00F5FF"
-              stop-opacity="0.593858"
-            />
-            <stop offset="1" stop-color="#01E7E7" stop-opacity="0" />
+            <stop stop-color="#193497" offset="0" />
+            <stop stop-color="#5180E4" stop-opacity=".18" offset="1" />
           </linearGradient>
-          <linearGradient
-            :id="`${uuid}_paint7_linear`"
-            x1="4833.31"
-            y1="24"
-            x2="4573.31"
-            y2="245"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#01F5F1" stop-opacity="0" />
-            <stop
-              offset="0.265077"
-              stop-color="#01F5F1"
-              stop-opacity="0.494147"
-            />
-            <stop offset="0.660232" stop-color="#01F5F1" />
-            <stop offset="0.852647" stop-color="#01F5F1" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient
-            :id="`${uuid}_paint8_linear`"
-            x1="2846.31"
-            y1="24"
-            x2="3106.31"
-            y2="245"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#01F5F1" stop-opacity="0" />
-            <stop
-              offset="0.265077"
-              stop-color="#01F5F1"
-              stop-opacity="0.494147"
-            />
-            <stop offset="0.660232" stop-color="#01F5F1" />
-            <stop offset="0.852647" stop-color="#01F5F1" stop-opacity="0" />
-          </linearGradient>
-          <clipPath :id="`${uuid}_clip0`">
-            <rect width="7680" height="220" fill="white" />
-          </clipPath>
         </defs>
       </svg>
     </div>
@@ -248,7 +193,6 @@ export default class Header extends Vue {
   height: 220px;
   text-align: center;
   color: #fff;
-  @extend %bg-img-header;
   .background {
     position: absolute;
     top: 0;
