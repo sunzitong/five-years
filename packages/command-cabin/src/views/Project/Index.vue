03aba0b194975a277091611c170f346805a0ace3
jinxiaochen <jinxiaochen@longfor.com>
2021-09-15 17:39:16 +0800

    feat: 门店大屏布局

--- packages/command-cabin/src/views/Project/Index.vue
+++ packages/command-cabin/src/views/Project/Index.vue
@@ -1,163 +1,112 @@
 <template>
   <div class="page__project">
-    <div class="header">龙湖冠寓重庆佳华北宸里店</div>
+    <WhiteSpace />
     <div class="main">
       <div class="main-left">
-        <SubWrapperA title="财务相关指标及预估">
-          <van-row gutter="24">
-            <van-col>
-              <!-- 资产类型 -->
-              <CardA style="width: 1098px; height: 560px">
-                <A1 />
-              </CardA>
-              <WhiteSpace />
-              <CardA :showFooter="true" style="width: 1098px; height: 560px">
-                <template #footer>
-                  <a>
-                    投资任务书
-                    <Icon type="arrow-right" color="#fff" />
-                  </a>
-                </template>
-                <A2 />
-              </CardA>
-            </van-col>
-            <van-col>
-              <CardA style="width: 1218px; height: 1145px"><A6 /></CardA>
-            </van-col>
-          </van-row>
+        <SubWrapperA title="基本信息" style="width: 1961px; height: 2972px">
+          <!-- 资产类型 -->
+          <CardA title="资产类型：重资产" style="width: 1909px; height: 1379px">
+            <A1 />
+          </CardA>
           <WhiteSpace />
-          <van-row gutter="24">
-            <van-col>
-              <CardA
-                title="经营团队"
-                size="small"
-                style="width: 764px; height: 408px"
-              >
-                <A3 />
-              </CardA>
-            </van-col>
-            <van-col>
-              <CardA
-                title="设计团队"
-                size="small"
-                style="width: 764px; height: 408px"
-              >
-                <A4 />
-              </CardA>
-            </van-col>
-            <van-col>
-              <CardA
-                title="施工团队"
-                size="small"
-                style="width: 764px; height: 408px"
-              >
-                <A5 />
-              </CardA>
-            </van-col>
-          </van-row>
+          <CardA style="width: 1909px; height: 429px">
+            <template #footer>
+              <a>
+                投资任务书
+                <Icon type="arrow-right" color="#fff" />
+              </a>
+            </template>
+            <A2 />
+          </CardA>
           <WhiteSpace />
-          <van-row>
-            <van-col>
-              <CardA
-                title="项目区位"
-                style="width: 2340px; height: 1115px"
-              ></CardA>
-            </van-col>
-          </van-row>
+          <CardA title="项目区位" style="width: 1909px; height: 956px"></CardA>
         </SubWrapperA>
       </div>
       <div class="main-center">
-        <SubWrapperA title="财务指标达成及预估">
-          <van-row gutter="24">
+        <SubWrapperA
+          title="财务相关指标及预估"
+          style="width: 2856px; height: 2740px"
+        >
+          <van-row gutter="20">
             <van-col>
-              <WhiteSpace />
-              <CardA style="width: 1506px; height: 2398px"></CardA>
+              <CardA
+                title="财务相关指标及预估"
+                style="width: 1590px; height: 2610px"
+              ></CardA>
             </van-col>
             <van-col>
-              <CardA title="综合经营指数" style="width: 1050px; height: 597px">
-                <C5 />
-              </CardA>
-              <WhiteSpace />
               <CardA
                 title="当前投资实现情况"
-                style="width: 1050px; height: 582px"
+                style="width: 1193px; height: 1611px"
               ></CardA>
               <WhiteSpace />
               <CardA
                 title="奖补到账"
-                style="width: 1050px; height: 260px"
+                style="width: 1193px; height: 246px"
               ></CardA>
+              <SubWrapperA
+                title="风险监控"
+                style="width: 1193px; height: 736px"
+              >
+                <CardA style="width: 1168px; height: 591px"></CardA>
+              </SubWrapperA>
             </van-col>
           </van-row>
         </SubWrapperA>
       </div>
       <div class="main-right">
-        <SubWrapperA title="经营现状">
-          <van-row gutter="24">
+        <SubWrapperA title="经营现状" style="width: 2693px; height: 2027px">
+          <van-row gutter="20">
             <van-col>
-              <CardA
-                title="全业态收入及完成度"
-                style="width: 1593px; height: 578px"
-              >
+              <CardA title="收入量价情况" style="width: 1820px; height: 1100px">
+                <template #data-cycle>{{ dataCycle }}</template>
+              </CardA>
+              <van-row gutter="20">
+                <van-col>
+                  <CardA
+                    title="综合经营指数"
+                    style="width: 900px; height: 420px"
+                  ></CardA>
+                  <WhiteSpace />
+                  <CardA
+                    title="24小时设备离线率"
+                    style="width: 900px; height: 310px"
+                  >
+                    <template #data-cycle>{{ dataCycle }}</template>
+                  </CardA>
+                </van-col>
+                <van-col>
+                  <CardA
+                    title="客户洞察"
+                    style="width: 900px; height: 750px"
+                  ></CardA>
+                </van-col>
+              </van-row>
+            </van-col>
+            <van-col>
+              <CardA title="经营性支出" style="width: 801px; height: 924px">
+                <template #data-cycle>{{ dataCycle }}</template>
+              </CardA>
+              <WhiteSpace />
+              <CardA title="逾期金额" style="width: 801px; height: 924px">
                 <template #data-cycle>{{ dataCycle }}</template>
               </CardA>
             </van-col>
-            <van-col>
-              <WhiteSpace />
-              <CardA style="width: 723px; height: 555px">
-                <D6 />
-              </CardA>
-            </van-col>
-          </van-row>
-          <WhiteSpace />
-          <van-row gutter="24">
-            <van-col>
-              <CardA
-                title="经营性支出"
-                style="width: 904px; height: 556px"
-              ></CardA>
-            </van-col>
-            <van-col>
-              <CardA title="逾期金额" style="width: 665px; height: 556px"></CardA>
-            </van-col>
-            <van-col>
-              <CardA title="舆情风险" style="width: 723px; height: 556px">
-                <D7 />
-              </CardA>
-            </van-col>
-          </van-row>
-          <WhiteSpace />
-          <van-row gutter="24">
-            <van-col>
-              <CardA title="客户洞察" style="width: 904px; height: 392px"></CardA>
-            </van-col>
-            <van-col>
-              <CardA
-                title="24小时设备离线率"
-                style="width: 665px; height: 392px"
-              >
-                <D5 />
-              </CardA>
-            </van-col>
-            <van-col>
-              <CardA
-                title="能源费用(收支差)"
-                style="width: 723px; height: 312px"
-              >
-                <D8 />
-              </CardA>
-            </van-col>
-          </van-row>
-          <WhiteSpace />
-          <van-row gutter="24">
-            <van-col>
-              <CardA
-                title="风险预警"
-                style="width: 2340px; height: 1114px"
-              ></CardA>
-            </van-col>
           </van-row>
         </SubWrapperA>
+        <WhiteSpace />
+        <van-row>
+          <van-col>
+            <SubWrapperA title="风险预警" style="width: 1846px; height: 920px">
+              <CardA style="width: 1794px; height: 764px"></CardA>
+            </SubWrapperA>
+          </van-col>
+          <van-col>
+            <SubWrapperA title="舆情风险" style="width: 828px; height: 920px">
+              <CardA style="width: 776px; height: 777px"></CardA>
+            </SubWrapperA>
+          </van-col>
+        </van-row>
       </div>
     </div>
     <div class="footer">
@@ -266,31 +215,13 @@ export default class Index extends Base {
 .page__project {
   width: 7680px;
   color: #fff;
-  .header {
-    height: 120px;
-    line-height: 120px;
-    font-size: 60px;
-    text-align: center;
-    margin-bottom: 45px;
-  }
   .main {
     display: flex;
     margin: 0 60px;
   }
-  .main-right,
-  .main-left {
-    max-width: 2420px;
-    min-width: 2420px;
-  }
   .main-center {
     flex: 1;
-    margin: 0 40px;
-  }
-
-  .main-left-row {
-    padding: 20px;
-    display: flex;
-    justify-content: space-between;
+    margin: 0 20px;
   }
 }
 .footer {
