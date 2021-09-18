af6896713df18103064f3796d24f34f2a5256e3f
jinxiaochen <jinxiaochen@longfor.com>
2021-09-18 16:32:22 +0800

    feat: add D1B

--- packages/command-cabin/src/views/Project/components/D1B.vue
+++ packages/command-cabin/src/views/Project/components/D1B.vue
@@ -1,52 +1,60 @@
 <template>
   <div class="operation">
+    <div class="line"></div>
     <van-row>
-      <van-col span="12">
-        <h4 class="label">平均出租率</h4>
+      <van-col span="10">
+        <h4 class="label">平均出租率(开业三月以上)</h4>
         <div class="">
           <strong>95.3%</strong>
-          同比
-          <em>43.2%</em>
+          环比
+          <strong class="desc">95.3%</strong>
+          <span class="icon">
+            <Icon type="desc" color="#22cb98" size="62" />
+          </span>
         </div>
         <div class="pointOfTime">时点 84.2%</div>
       </van-col>
-      <van-col span="12">
+      <van-col span="14">
         <h4 class="label">平均续租率</h4>
         <div class="">
           <strong>95.3%</strong>
-          同比
-          <span class="icon"><Icon type="desc" /></span>
-          <em>43.2%</em>
-        </div>
-      </van-col>
-      <van-col span="24" style="padding-top: 30px">
-        <h4 class="label">在租房间平均成交价(元)</h4>
-        <div class="">
+          环比
+          <span class="icon">
+            <Icon type="desc" color="#22cb98" size="62" />
+          </span>
           <strong>95.3%</strong>
-          同比
-          <span class="icon"><Icon type="desc" /></span>
-          <em>43.2%</em>
         </div>
       </van-col>
     </van-row>
-    <div class="line"></div>
-    <van-row type="flex" align="bottom">
-      <van-col span="24">
+    <van-row>
+      <van-col span="10" style="padding-top: 30px">
         <h4 class="label">同房间价格涨幅</h4>
+        <div class="">
+          <strong>95.3%</strong>
+          同比
+          <span class="icon">
+            <Icon type="desc" color="#22cb98" size="62" />
+          </span>
+        </div>
       </van-col>
-      <van-col span="8">
-        <span class="icon"><Icon type="desc" /></span>
-        <strong>2.1%</strong>
+      <van-col span="5" style="padding-top: 30px">
+        <h4 class="label">在租房间平均成交价(元)</h4>
+        <div class="">
+          <strong>95.3%</strong>
+          <span class="icon">
+            <Icon type="asce" color="#FF3980" size="62" />
+          </span>
+        </div>
       </van-col>
-      <van-col span="8">
-        <span class="icon"><Icon type="asce" /></span>
-        <em class="asce">2.1%</em>
-        新签
-      </van-col>
-      <van-col span="8">
-        <span class="icon"><Icon type="asce" /></span>
-        <em class="asce">2.1%</em>
-        续租
+      <van-col span="5" style="padding-top: 30px">
+        <div class="simple">
+          <span class="label">新签上涨:</span>
+          <strong>100%</strong>
+        </div>
+        <div class="simple">
+          <span class="label">续租下降:</span>
+          <strong>100%</strong>
+        </div>
       </van-col>
     </van-row>
   </div>
@@ -70,7 +78,9 @@ export default class D1B extends Base {
 </script>
 
 <style lang="scss" scoped>
-$light: #01f5f1;
+$light: rgba(219, 240, 255, 1);
+$desc: #22cb98;
+$asce: #ff2a76;
 
 .operation {
   padding: 0 43px;
@@ -86,29 +96,41 @@ $light: #01f5f1;
     background-size: 20px 2px;
     background-repeat: repeat-x;
   }
+
   .label {
     font-size: 30px;
     font-weight: normal;
-    color: #fff;
+    color: rgba(144, 164, 195, 1);
   }
-  strong,
-  em {
+
+  strong {
     font-style: normal;
+    font-size: 66px;
     color: $light;
     @extend %value__letter;
   }
-  strong {
-    font-size: 40px;
+
+  .simple {
+    display: flex;
+    align-items: center;
+    padding-left: 27px;
+    line-height: 58px;
+    border-left: 2px solid #5180e4;
+    background: linear-gradient(
+      89.96deg,
+      rgba(5, 203, 253, 0.1) 0%,
+      rgba(0, 127, 249, 0) 100.9%
+    );
+    strong {
+      margin-left: 10px;
+      font-size: 40px;
+    }
   }
-  em {
-    font-size: 30px;
-  }
+
   .icon {
     margin: 0 10px;
   }
-  .asce {
-    color: #ff2a76;
-  }
+
   .pointOfTime {
     display: inline-block;
     padding: 0 10px;
@@ -116,6 +138,13 @@ $light: #01f5f1;
     border-radius: 20px;
     color: #fff;
     font-size: 20px;
+  }
+
+  .desc {
+    color: $desc;
+  }
+  .asce {
+    color: $asce;
   }
 }
 </style>
