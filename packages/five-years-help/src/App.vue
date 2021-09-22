501bd5716431da97b1d4378e1c81d32a95fb4a99
zhouwei <zhouwei201@longfor.com>
2021-09-22 17:14:27 +0800

    feat: debug 分享

--- packages/five-years-help/src/App.vue
+++ packages/five-years-help/src/App.vue
@@ -33,7 +33,8 @@ export default class App extends Mixins(MixStore) {
    */
   @Provide()
   share(url: string) {
-    const title = "冠寓五周年,快乐助我赚珑珠!";
+    // const title = "冠寓五周年,快乐助我赚珑珠!";
+    const title = url;
     const img =
       "https://guanyuoss.oss-cn-qingdao.aliyuncs.com/prod/app/H_CvGHPIaeZkvNegO0Glbw.png";
     if (this.visitSource === "小程序") {
