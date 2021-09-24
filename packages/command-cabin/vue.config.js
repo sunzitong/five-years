3333b23bd56ab0becddbc22ef27060feb5b446b0
xiejiabin <xiejiabin@longfor.com>
2021-09-24 16:02:43 +0800

    preload

--- packages/command-cabin/vue.config.js
+++ packages/command-cabin/vue.config.js
@@ -25,8 +25,8 @@ module.exports = {
   // 删除 HTML 相关的 webpack 插件
   chainWebpack: (config) => {
     // config.plugins.delete('html')
-    config.plugins.delete("preload");
-    config.plugins.delete("prefetch");
+    // config.plugins.delete("preload");
+    // config.plugins.delete("prefetch");
   },
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === "production") {
