3377472bca0f745477563e5145f64bfaa6cca334
xiejiabin <xiejiabin@longfor.com>
2021-09-24 17:02:35 +0800

    test: 优化

--- packages/command-cabin/src/App.vue
+++ packages/command-cabin/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ 'app--bg': !appLoading }">
     <div v-show="$route.meta.name !== 'login'">
       <div class="logo"></div>
       <AppHeader />
