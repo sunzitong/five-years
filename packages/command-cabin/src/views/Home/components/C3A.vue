0c898c12f503b00a8e63675f5c69a6d013caef68
jinxiaochen <jinxiaochen@longfor.com>
2021-10-11 15:27:24 +0800

    feat: 修改弹窗

--- packages/command-cabin/src/views/Home/components/C3A.vue
+++ packages/command-cabin/src/views/Home/components/C3A.vue
@@ -3,88 +3,106 @@
     <van-dialog
       class="dialog"
       v-model="show"
-      width="3015"
+      width="3020"
       :show-confirm-button="false"
       :get-container="() => $root.$el"
     >
       <SubWrapperA title="现场风险监控" style="width: 3015">
-        <van-row>
-          <van-col span="18">
-            <CardA
-              opacity="1"
-              fill="rgba(8, 18, 35, 1)"
-              title="现场监控"
-              style="height: 1348px"
-            >
-              <Spin :loading="loading" :empty="empty">
+        <CardA opacity="1" fill="rgba(8, 18, 35, 1)">
+          <van-row gutter="40">
+            <van-col span="18">
+              <div class="box">
+                <div class="carda-title">
+                  <span>现场风险监控</span>
+                </div>
                 <div class="pictures-wrapper">
                   <Webrtc :config="webrtcConfig" />
                 </div>
-              </Spin>
-            </CardA>
-            <WhiteSpace />
-            <CardA
-              opacity="1"
-              fill="rgba(8, 18, 35, 1)"
-              title="问题现场照片"
-              style="height: 625px"
-            >
-              <div class="gallery">
-                <div
-                  class="item"
-                  @click="() => swipe.swipeTo(item.index)"
-                  v-for="(item, index) of pageContent.list"
-                  :key="index"
-                >
-                  <van-image width="707" height="397" :src="item"></van-image>
+              </div>
+            </van-col>
+            <van-col span="6">
+              <div class="box">
+                <div class="carda-title">
+                  <span>问题描述</span>
+                </div>
+                <div class="pictrues-info">
+                  <h3>{{ response.projectName }}</h3>
+                  <ul>
+                    <li>
+                      <div class="label">位置区域：</div>
+                      <div class="con">暂未提供</div>
+                    </li>
+                    <li>
+                      <div class="label">问题描述：</div>
+                      <div class="con">{{ response.problem }}</div>
+                    </li>
+                    <li>
+                      <div class="label">发现时间：</div>
+                      <div class="con">{{ response.showTime }}</div>
+                    </li>
+                    <li>
+                      <div class="label">整改时间：</div>
+                      <div class="con">{{ response.reformTime }}</div>
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div class="pagination">
-                <Pagination
-                  :total="pageContent.total"
-                  :pages="Math.ceil(pageContent.total / 3)"
-                  v-model="pageNum"
-                />
+            </van-col>
+          </van-row>
+          <WhiteSpace />
+          <van-row gutter="40">
+            <van-col span="18">
+              <div class="box">
+                <div class="carda-title">
+                  <span>问题现场照片</span>
+                </div>
+                <div class="gallery">
+                  <div
+                    class="item"
+                    @click="() => swipe.swipeTo(item.index)"
+                    v-for="(item, index) of pageContent.list"
+                    :key="index"
+                  >
+                    <van-image width="707" height="397" :src="item"></van-image>
+                  </div>
+                </div>
+                <div class="pagination">
+                  <Pagination
+                    :total="pageContent.total"
+                    :pages="Math.ceil(pageContent.total / 3)"
+                    v-model="pageNum"
+                  />
+                </div>
               </div>
-            </CardA>
-          </van-col>
-          <van-col span="6">
-            <CardA
-              opacity="1"
-              fill="rgba(8, 18, 35, 1)"
-              title="问题描述"
-              style="height: 1348px"
-            ></CardA>
-            <WhiteSpace />
-            <CardA
-              opacity="1"
-              fill="rgba(8, 18, 35, 1)"
-              title="联系人"
-              style="height: 625px"
-            ></CardA>
-          </van-col>
-        </van-row>
+            </van-col>
+            <van-col span="6">
+              <div class="box">
+                <div class="carda-title">
+                  <span>联系人</span>
+                </div>
+                <div class="pictrues-info contacts">
+                  <ul>
+                    <li v-for="person of contacts" :key="person.phone">
+                      <div class="label">{{ person.roleName }}：</div>
+                      <div class="con">
+                        {{ person.name }} {{ person.phone }}
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </van-col>
+          </van-row>
+        </CardA>
         <CardA fill="rgba(8, 18, 35, 1)" opacity="1">
-          <div class="btns">
-            <div
-              class="btn"
-              :class="{ disabled: currentIndex === 0 }"
-              @click="prevOrderDetail"
-            >
-              <van-icon name="arrow-left" />
-            </div>
-            <div
-              class="btn"
-              :class="{ disabled: currentIndex === response.length - 1 }"
-              @click="nextOrderDetail"
-            >
-              <van-icon name="arrow" />
-            </div>
-          </div>
           <div class="closes">
             <span class="cross" @click="toggleDialog">
               <van-icon name="cross" />
             </span>
+            <div class="btns">
+              <div class="btn" @click="prevOrderDetail">上一个</div>
+              <div class="btn" @click="nextOrderDetail">下一个</div>
+            </div>
             <span class="cross" @click="toggleDialog">
               <van-icon name="cross" />
             </span>
@@ -140,11 +158,6 @@ export default class C3A extends Base implements IFetch {
   @Prop({ default: () => [] }) dataSource!: MonitorListItemReturn[];
 
   /**
-   * 当前索引
-   */
-  currentIndex = 0;
-
-  /**
    * 当前页
    */
   pageNum = 1;
@@ -155,9 +168,15 @@ export default class C3A extends Base implements IFetch {
   orderIdList: number[] = [];
 
   /**
+   * 工单id索引
+   */
+  orderIdIndex = 0;
+
+  /**
    * 联系人
    */
   get contacts() {
+    console.log(this.response.contacts);
     return iwant.array(this.response.contacts);
   }
 
@@ -177,13 +196,13 @@ export default class C3A extends Base implements IFetch {
    * 播放器配置
    */
   get webrtcConfig() {
-    const item = this.response[this.currentIndex] ?? {};
+    const item = iwant.object(this.response) ?? {};
     return {
       width: 2181,
       height: 1226,
       autoplay: true,
-      poster: item.imageUrl,
-      cameraId: item.cameraId,
+      poster: item.monitorId,
+      cameraId: item.monitorId,
     };
   }
 
@@ -192,7 +211,15 @@ export default class C3A extends Base implements IFetch {
    */
   @Watch("dataSource", { deep: true })
   onDataSourceChange(val: MonitorListItemReturn[]) {
+    console.log("dataSource", val);
     this.orderIdList = iwant.array(val).map((item) => item.orderId);
+  }
+
+  /**
+   * 获取当前工单id
+   */
+  getCurrentOrderId() {
+    return this.orderIdList[this.orderIdIndex];
   }
 
   /**
@@ -217,14 +244,27 @@ export default class C3A extends Base implements IFetch {
    * 下一个工单
    */
   nextOrderDetail() {
-    console.log("nextOrderDetail");
+    if (this.orderIdList.length === 0) return;
+    if (this.orderIdIndex === -1) {
+      this.orderIdIndex = this.orderIdList.length - 1;
+    } else {
+      this.orderIdIndex -= 1;
+    }
+
+    console.log("this.nextOrderDetail", this.orderIdIndex);
   }
 
   /**
    * 上一个工单
    */
   prevOrderDetail() {
-    console.log("prevOrderDetail");
+    if (this.orderIdList.length === 0) return;
+    if (this.orderIdIndex === this.orderIdList.length) {
+      this.orderIdIndex = 0;
+    } else {
+      this.orderIdIndex += 1;
+    }
+    console.log("this.nextOrderDetail", this.orderIdIndex);
   }
 
   /**
@@ -236,12 +276,16 @@ export default class C3A extends Base implements IFetch {
    * 获取工单详情
    */
   async fetch() {
+    const orderId = this.getCurrentOrderId();
+    // if (!orderId) return;
+
     const response = await useStore(fetchMonitorInfo, {
       key: StoreKey.HomeMonitorInfo,
       params: {
-        orderId: 12345,
+        orderId,
       },
     });
+
     if (response?.status === "ok") {
       this.response = iwant.object(response.data);
       this.empty = false;
@@ -260,11 +304,14 @@ export default class C3A extends Base implements IFetch {
     margin: 30px;
   }
   .closes {
+    margin-top: 20px;
     padding: 30px 40px 10px;
     display: flex;
     justify-content: space-between;
+    align-items: flex-end;
   }
   .cross {
+    cursor: pointer;
     font-size: 60px;
     color: #fff;
   }
@@ -274,21 +321,26 @@ export default class C3A extends Base implements IFetch {
   display: flex;
   width: 500px;
   justify-content: center;
-  margin: 20px auto 0;
+  padding: 40px;
   .btn {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 120px;
-    height: 66px;
+    width: 292px;
+    height: 80px;
     margin: 0 5px;
-    background: rgba(56, 196, 255, 0.3);
     backdrop-filter: blur(20px);
-    color: #01f5f1;
-    font-size: 50px;
+    color: #fff;
+    font-size: 36px;
+    cursor: pointer;
+    background: linear-gradient(
+      279.3deg,
+      rgba(81, 128, 228, 0.2) 6.65%,
+      rgba(81, 128, 228, 0.2) 92.58%
+    );
   }
   .btn:nth-child(1) {
-    margin-right: 300px;
+    margin-right: 80px;
   }
   .disabled {
     background: rgba(56, 196, 255, 0.1);
@@ -299,7 +351,6 @@ export default class C3A extends Base implements IFetch {
 .pictures-wrapper {
   position: relative;
   margin: 0 auto;
-  padding-top: 25px;
 }
 
 .pictures {
@@ -322,7 +373,7 @@ export default class C3A extends Base implements IFetch {
 
 .gallery {
   display: flex;
-  padding: 30px;
+  margin-bottom: 40px;
   justify-content: space-between;
   .label {
     height: 70px;
@@ -333,14 +384,63 @@ export default class C3A extends Base implements IFetch {
     background: #1a212c;
   }
 }
+
 .pagination {
   display: flex;
-  padding: 20px;
+  margin-bottom: 40px;
   justify-content: flex-end;
 }
+
 .pagination::v-deep {
   .page__total {
     font-size: 40px;
+  }
+}
+
+.pictrues-info {
+  > h3 {
+    padding: 30px 0;
+    font-size: 40px;
+  }
+  li {
+    margin-top: 20px;
+    display: flex;
+    color: #e4f8ff;
+    font-size: 36px;
+    line-height: 50px;
+    align-items: center;
+    .label {
+      color: #80a4c9;
+    }
+  }
+  &.contacts {
+    overflow: hidden;
+    min-height: 400px;
+    padding-left: 30px;
+    border-left: 2px solid #5180e4;
+    background: linear-gradient(
+      90deg,
+      rgba(5, 203, 253, 0.1) 0.19%,
+      rgba(0, 127, 249, 0) 98.66%
+    );
+  }
+}
+
+.carda-title {
+  height: 90px;
+  border-bottom: 1px solid #1b3148;
+  margin-bottom: 30px;
+  span {
+    display: inline-block;
+    padding: 16px 52px;
+    color: #5180e4;
+    font-size: 40px;
+    border-bottom: 2px solid #5180e4;
+    background: linear-gradient(
+      180deg,
+      rgba(83, 214, 255, 0) 0%,
+      rgba(81, 128, 228, 0.1) 100%
+    );
   }
 }
 </style>
