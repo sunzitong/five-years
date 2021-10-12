ab3ca1a1d88dfc0e981d2b362ca8cc1d1d054b71
jingxiaozhu <jingxiaozhu@longfor.com>
2021-10-12 16:18:59 +0800

    fix: 缺字段兼容

--- packages/command-cabin/src/views/Project/components/B1B.vue
+++ packages/command-cabin/src/views/Project/components/B1B.vue
@@ -203,7 +203,7 @@ export default class B1B extends Base {
       },
       grid: {
         top: "28%",
-        left: "3%",
+        left: "8%",
         right: "6%",
         bottom: "8%",
         //是否显示刻度标签
