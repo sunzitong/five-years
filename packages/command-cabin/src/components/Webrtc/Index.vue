06a6a2ca0edd219f59c57f3d6ae10cb9e3d4d5dc
jinxiaochen <jinxiaochen@longfor.com>
2021-10-12 11:05:55 +0800

    feat: 视频播放

--- packages/command-cabin/src/components/Webrtc/Index.vue
+++ packages/command-cabin/src/components/Webrtc/Index.vue
@@ -2,13 +2,14 @@
   <div ref="webrtc" class="app-webrtc"></div>
 </template>
 <script lang="ts">
+import { Base } from "@/views/Base";
 import { getEnvHost, iwant } from "@guanyu/shared";
-import { Component, Prop, Ref, Vue, Watch } from "vue-property-decorator";
+import { Component, Prop, Ref, Watch } from "vue-property-decorator";
 import { PlayerConfig } from "./webrtc.type";
 export type { PlayerConfig } from "./webrtc.type";
 
 @Component
-export default class Webrtc extends Vue {
+export default class Webrtc extends Base {
   /**
    * 容器
    */
@@ -20,13 +21,6 @@ export default class Webrtc extends Vue {
   @Prop() config!: PlayerConfig;
 
   /**
-   * 组件挂载
-   */
-  mounted() {
-    // this.initPlayer();
-  }
-
-  /**
    * 初始化webrtc 播放器
    */
   @Watch("config", { deep: true, immediate: true })
@@ -35,28 +29,19 @@ export default class Webrtc extends Vue {
   }
 
   /**
-   * 用户数据更新
-   */
-  @Watch("store.currentUser", { deep: true, immediate: true })
-  onCurrentUserChange() {
-    this.initPlayer(this.config);
-  }
-
-  /**
    * 初始化播放器
    */
   initPlayer(val?: PlayerConfig) {
     this.$nextTick(() => {
       const user = iwant.object(this.store?.currentUser);
-      if (!user.phone && !user.oaAccount) return;
       const config = {
         accessChannel: "huiyan",
         watermark: `${user.oaAccount} ${user.phone}`,
         autoplay: false,
         // cameraId: "00bcf8bbe82e0c36daa781e8568cd7d4",
+        // cameraId: "0002c6450e0cd4025ef3ba3a9c712aac",
         env: getEnvHost() === "production" ? "prod" : "uat",
         ...(val ?? {}),
-        cameraId: "0002c6450e0cd4025ef3ba3a9c712aac",
       };
       this.destroyPlayer();
       console.log(getEnvHost());
