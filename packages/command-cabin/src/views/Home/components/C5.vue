f95d82fbf46ac7f6bd1e53c30c6ab45044b87bd5
xiejiabin <xiejiabin@longfor.com>
2021-10-12 14:26:27 +0800

    feat: C5大事记

---
+++ packages/command-cabin/src/views/Home/components/C5.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">sssss</div>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import { Base, IFetch } from "@/views/Base";
+import StepNumber from "@/components/StepNumber/Index.vue";
+import CardB from "@/components/CardB/Index.vue";
+import Animationend from "@/components/Animationend/Index.vue";
+
+@Component({
+  components: {
+    StepNumber,
+    CardB,
+    Animationend,
+  },
+})
+export default class C5 extends Base implements IFetch {
+  async fetch() {
+    return;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  position: relative;
+  height: 1858px;
+  --dart: #0e173c;
+  --light: rgba(14, 23, 60, 0.5);
+  background: red;
+}
+</style>
