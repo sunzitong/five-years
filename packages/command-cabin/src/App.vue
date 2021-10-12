65296d264acd7672431305f78731da3c09ae6161
jinxiaochen <jinxiaochen@longfor.com>
2021-10-12 17:31:10 +0800

    feat: 添加水印

--- packages/command-cabin/src/App.vue
+++ packages/command-cabin/src/App.vue
@@ -199,8 +199,7 @@ export default class App extends Mixins(MixStore) {
       this.store.global.project = resProjectList.data[0];
       this.appLoading = false;
     }
-    // window.MAIAAPM?.setUid(this.store.currentUser?.userId);
-    window.MAIAAPM?.setUid(88888888);
+    window.MAIAAPM?.setUid(this.store.currentUser?.userId);
   }
 
   /**
