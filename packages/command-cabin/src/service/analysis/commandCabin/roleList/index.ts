02369c02fb73451ae2a9515d6f7313b64df71ce5
xiejiabin <xiejiabin@longfor.com>
2021-10-13 13:13:39 +0800

    feat: 获取角色列表 api

---
+++ packages/command-cabin/src/service/analysis/commandCabin/roleList/index.ts
@@ -0,0 +1,28 @@
+/**
+ * 本文件自动生成,勿手动更改,如需修改可以在同目录下进行扩展
+ * 接口文档: http://docs.gyapt.cn/project/712/interface/api/115016
+ */
+
+import http, { ServiceOptions } from "@/service/http";
+const BASE_URL = process.env.VUE_APP_BASE_API;
+
+/**
+ * 获取角色列表-返回值
+ */
+
+/**
+ * 获取角色列表
+ * @createBy huyanan
+ * @updateAt 2021/10/13 12:32:55
+ * @method GET
+ */
+export const fetchRoleList = (
+  params?: Record<string, unknown>,
+  options?: Partial<ServiceOptions>
+) => {
+  return http.get<RoleListItemReturn[]>(
+    `${BASE_URL}/analysis/commandCabin/roleList`,
+    { ...params },
+    { ...options }
+  );
+};
